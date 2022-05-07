c2c52a4f98870e6daa64bee1cce71cb66c566288
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-05-07 14:57:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline center">Hello world!</h1>;
+      <button className="bg-red-500 rounded">test</button>
+    </div>
+  );
+};
+
+export default App;
